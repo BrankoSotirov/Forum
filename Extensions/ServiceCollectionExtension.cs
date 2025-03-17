a7b104c92baa8fd0331aeafa93e7a74429e41ab9
brank <brankosotirov@gmail.com>
2025-03-17 19:32:10 +0200

    Simplyfing Program.cs class Extension added

---
+++ Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,38 @@
+﻿using Forum.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Authentication.ExtendedProtection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static  class ServiceCollectionExtension
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+        public static IServiceCollection AddApplicationDbcontext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+
+           services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
